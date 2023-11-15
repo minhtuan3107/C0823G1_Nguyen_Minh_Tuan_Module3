1ec7c917c258381496e5483c5f33ce28d3403820
minhtuan3107 <minhtuan3107+tn989993@users.noreply.github.com>
2023-11-15 10:27:46 +0700

    nop bai tap ss1 co so du lieu quan he

--- ss1_co_so_du_lieu_quan_he/baitap/baitap1/baitap1.sql
+++ ss1_co_so_du_lieu_quan_he/baitap/baitap1/baitap1.sql
@@ -1,17 +1,23 @@
 CREATE DATABASE `baitap1`;
 use baitap1;
 
-create table class(
-	id int key auto_increment,
-    name varchar(200)
+CREATE TABLE class (
+    id INT KEY AUTO_INCREMENT,
+    name VARCHAR(200)
 );
-create table teacher(
-	id int key auto_increment,
-	name varchar(200),
-	age int,
-	country varchar(200)
+CREATE TABLE teacher (
+    id INT KEY AUTO_INCREMENT,
+    name VARCHAR(200),
+    age INT,
+    country VARCHAR(200)
 );
 insert into class(name) value("Tuấn");
-select * from class;
+SELECT 
+    *
+FROM
+    class;
 insert into teacher(name,age,country) value("Tuan",18,"VietNam"),("Hehe",19,"Viet Nam");
-select * from teacher;
+SELECT 
+    *
+FROM
+    teacher;
