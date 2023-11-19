e080674dee7464d717dabf5db6241efff98ac5ba
minhtuan3107 <minhtuan3107+tn989993@users.noreply.github.com>
2023-11-19 19:09:00 +0700

    nop case study

--- casestudy/query_furama.sql
+++ casestudy/query_furama.sql
@@ -30,7 +30,7 @@ order by so_lan;
 -- là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra). 
 
 select kh.ma_khach_hang, kh.ho_ten,lk.ten_loai_khach,hd.ma_hop_dong,dv.ten_dich_vu,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc,
-(IFNULL(dv.chi_phi_thue, 0) + SUM(IFNULL(hdct.so_luong, 0) * IFNULL(dvdk.gia, 0))) as 'tong_tien'
+(ifnull(dv.chi_phi_thue, 0) + sum(ifnull(hdct.so_luong, 0) * ifnull(dvdk.gia, 0))) as 'tong_tien'
 from khach_hang kh
 join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
 left join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
@@ -38,3 +38,66 @@ left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
 left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
 left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
 group by kh.ma_khach_hang , hd.ma_hop_dong;
+
+ -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+ 
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+where hd.ngay_lam_hop_dong > '2021-04-01'
+group by dv.ma_dich_vu;
+
+-- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm alte
+-- nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+
+select dv.ma_dich_vu,dv.ten_dich_vu,dv.dien_tich,dv.so_nguoi_toi_da,dv.chi_phi_thue,ldv.ten_loai_dich_vu
+from dich_vu dv
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+where year(hd.ngay_lam_hop_dong) = 2020 and dv.ma_dich_vu not in (
+select ma_dich_vu
+from hop_dong
+where year(ngay_lam_hop_dong) = 2021);
+  
+  -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+  
+  
+-- cach 1
+
+select kh.ho_ten
+from khach_hang kh
+group by kh.ho_ten
+having count(*) = 1;
+
+-- cach 2 
+
+select ho_ten, count(*)
+from khach_hang
+group by ho_ten;
+
+-- cach 3
+
+select ho_ten
+from khach_hang
+group by ho_ten;
+
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+select thang as thang,count(*) as số_lượng_khách_hàng
+from(
+select month(ngay_lam_hop_dong) AS thang
+from hop_dong
+where year(ngay_lam_hop_dong) = 2021
+) as hd_thang
+group by thang;
+  
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, 
+-- ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+select hd.ma_hop_dong , ngay_lam_hop_dong, ngay_ket_thuc,tien_dat_coc,
+count(distinct dvdk.ma_dich_vu_di_kem) as số_lượng_dịch_vụ_đi_kèm
+from hop_dong hd
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by hd.ma_hop_dong;
